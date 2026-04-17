7ce96f6b9ee1b4265bd7cda3ecc4a29534cbb352
aswincp <aswincp554@gmail.com>
2026-04-17 16:27:53 +0530

    fixed the helmet issue

--- Backend/infrastructure/plugins/fastifyHelmet.ts
+++ Backend/infrastructure/plugins/fastifyHelmet.ts
@@ -1,25 +1,38 @@
 import helmet from '@fastify/helmet';
 import { FastifyInstance } from 'fastify';
 import { getHelmetFrameParticipantSources } from '../config/corsOrigins.js';
+import { env } from '../config/env.js';
 
 export async function registerHelmet(fastify: FastifyInstance): Promise<void> {
     const frameParticipants = getHelmetFrameParticipantSources();
+    const s3BucketHost = env.AWS_S3_BUCKET ? `https://${env.AWS_S3_BUCKET}.s3.${env.AWS_REGION ?? 'ap-south-1'}.amazonaws.com` : null;
+    const stripeScriptHosts = ['https://js.stripe.com'];
+    const stripeConnectHosts = ['https://api.stripe.com', 'https://r.stripe.com', 'https://m.stripe.network', 'https://q.stripe.com'];
+    const allowedImageHosts = ["'self'", 'data:', 'blob:', 'https:'];
+
+    if (s3BucketHost) {
+        allowedImageHosts.push(s3BucketHost);
+    }
 
     await fastify.register(helmet, {
         contentSecurityPolicy: {
+            useDefaults: false,
             directives: {
                 defaultSrc: ["'self'"],
                 baseUri: ["'self'"],
                 objectSrc: ["'none'"],
+                formAction: ["'self'"],
                 styleSrc: ["'self'", "'unsafe-inline'"],
-                scriptSrc: ["'self'", 'https://js.stripe.com'],
-                scriptSrcElem: ["'self'", 'https://js.stripe.com'],
-                imgSrc: ["'self'", 'data:', 'blob:', 'https:'],
+                scriptSrc: ["'self'", ...stripeScriptHosts],
+                scriptSrcElem: ["'self'", ...stripeScriptHosts],
+                imgSrc: allowedImageHosts,
                 fontSrc: ["'self'", 'data:', 'https:'],
-                connectSrc: ["'self'", 'https://api.stripe.com', 'https://r.stripe.com'],
-                frameSrc: [...frameParticipants, 'https://js.stripe.com', 'https://hooks.stripe.com'],
+                connectSrc: ["'self'", ...stripeConnectHosts],
+                frameSrc: [...frameParticipants, ...stripeScriptHosts, 'https://hooks.stripe.com'],
                 frameAncestors: frameParticipants,
                 workerSrc: ["'self'", 'blob:'],
+                manifestSrc: ["'self'"],
+                upgradeInsecureRequests: [],
             },
         },
         crossOriginEmbedderPolicy: false,
