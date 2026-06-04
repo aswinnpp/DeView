abb5a9f768a7dab4a91a810d7ad5bd57a47e0683
aswincp <aswincp554@gmail.com>
2026-06-04 19:40:55 +0530

    refactor(config): move hardcoded values to environment variables

--- Backend/infrastructure/plugins/fastifyHelmet.ts
+++ Backend/infrastructure/plugins/fastifyHelmet.ts
@@ -3,12 +3,32 @@ import { FastifyInstance } from 'fastify';
 import { getHelmetFrameParticipantSources } from '../config/corsOrigins.js';
 import { env } from '../config/env.js';
 
-export async function registerHelmet(fastify: FastifyInstance): Promise<void> {
+export async function registerHelmet(
+    fastify: FastifyInstance
+): Promise<void> {
     const frameParticipants = getHelmetFrameParticipantSources();
-    const s3BucketHost = env.AWS_S3_BUCKET ? `https://${env.AWS_S3_BUCKET}.s3.${env.AWS_REGION }.amazonaws.com` : null;
-    const stripeScriptHosts = ['https://js.stripe.com'];
-    const stripeConnectHosts = ['https://api.stripe.com', 'https://r.stripe.com', 'https://m.stripe.network', 'https://q.stripe.com'];
-    const allowedImageHosts = ["'self'", 'data:', 'blob:', 'https:'];
+
+    const s3BucketHost = env.AWS_S3_BUCKET
+        ? `https://${env.AWS_S3_BUCKET}.s3.${env.AWS_REGION}.amazonaws.com`
+        : null;
+
+    const stripeScriptHosts = [
+        'https://js.stripe.com',
+    ];
+
+    const stripeConnectHosts = [
+        'https://api.stripe.com',
+        'https://r.stripe.com',
+        'https://m.stripe.network',
+        'https://q.stripe.com',
+    ];
+
+    const allowedImageHosts = [
+        "'self'",
+        'data:',
+        'blob:',
+        'https:',
+    ];
 
     if (s3BucketHost) {
         allowedImageHosts.push(s3BucketHost);
@@ -22,21 +42,58 @@ export async function registerHelmet(fastify: FastifyInstance): Promise<void> {
                 baseUri: ["'self'"],
                 objectSrc: ["'none'"],
                 formAction: ["'self'"],
-                styleSrc: ["'self'", "'unsafe-inline'"],
-                scriptSrc: ["'self'", ...stripeScriptHosts],
-                scriptSrcElem: ["'self'", ...stripeScriptHosts],
+
+                styleSrc: [
+                    "'self'",
+                    "'unsafe-inline'",
+                ],
+
+                scriptSrc: [
+                    "'self'",
+                    ...stripeScriptHosts,
+                ],
+
+                scriptSrcElem: [
+                    "'self'",
+                    ...stripeScriptHosts,
+                ],
+
                 imgSrc: allowedImageHosts,
-                fontSrc: ["'self'", 'data:', 'https:'],
-                connectSrc: ["'self'", ...stripeConnectHosts],
-                frameSrc: [...frameParticipants, ...stripeScriptHosts, 'https://hooks.stripe.com'],
+
+                fontSrc: [
+                    "'self'",
+                    'data:',
+                    'https:',
+                ],
+
+                connectSrc: [
+                    "'self'",
+                    ...stripeConnectHosts,
+                    ...(s3BucketHost ? [s3BucketHost] : []),
+                ],
+
+                frameSrc: [
+                    ...frameParticipants,
+                    ...stripeScriptHosts,
+                    'https://hooks.stripe.com',
+                ],
+
                 frameAncestors: frameParticipants,
-                workerSrc: ["'self'", 'blob:'],
+
+                workerSrc: [
+                    "'self'",
+                    'blob:',
+                ],
+
                 manifestSrc: ["'self'"],
+
                 upgradeInsecureRequests: [],
             },
         },
+
         crossOriginEmbedderPolicy: false,
         frameguard: false,
     });
+
     fastify.log.info('Security headers (Helmet) registered');
 }
